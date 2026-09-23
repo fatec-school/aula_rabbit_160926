1283acd3cec225972635edd2d92765f6deccaa86
nickolss <nickolasmaiaraujo@gmail.com>
2026-09-23 10:25:45 -0300

    feat(event): add event in service actions

--- src/main/java/com/nickolss/Produto/service/ProductService.java
+++ src/main/java/com/nickolss/Produto/service/ProductService.java
@@ -1,6 +1,7 @@
 package com.nickolss.Produto.service;
 
 import com.nickolss.Produto.entity.Product;
+import com.nickolss.Produto.event.ProductEvent;
 import com.nickolss.Produto.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
@@ -11,24 +12,30 @@ import java.util.Optional;
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private final ProductEvent productEvent;
 
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(ProductRepository productRepository, ProductEvent productEvent) {
         this.productRepository = productRepository;
+        this.productEvent = productEvent;
     }
 
     public List<Product> findAll() {
+        productEvent.send("ProductService: findAll called");
         return productRepository.findAll();
     }
 
     public Optional<Product> findById(Long id) {
+        productEvent.send("ProductService: findById called with id: " + id);
         return productRepository.findById(id);
     }
 
     public Product save(Product product) {
+        productEvent.send("ProductService: save called with product: " + product);
         return productRepository.save(product);
     }
 
     public Optional<Product> update(Long id, Product product) {
+        productEvent.send("ProductService: update called with id: " + id + " and product: " + product);
         return productRepository.findById(id)
                 .map(existingProduct -> {
                     existingProduct.setName(product.getName());
@@ -43,6 +50,7 @@ public class ProductService {
     }
 
     public boolean deleteById(Long id) {
+        productEvent.send("ProductService: deleteById called with id: " + id);
         if (!productRepository.existsById(id)) {
             return false;
         }
